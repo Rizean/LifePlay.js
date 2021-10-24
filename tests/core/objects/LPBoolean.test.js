4404412d4beda7fa1adc16491851aa292c141f36
Rizean <rizean@gmail.com>
2021-10-24 07:51:53 -0400

    100% coverage

--- tests/core/objects/LPBoolean.test.js
+++ tests/core/objects/LPBoolean.test.js
@@ -3,70 +3,77 @@ const Scene = require('../../../src/core/Scene')
 const LB = '\r\n'
 
 test('boolean.and with lhs/rhs chaining', async () => {
-    let scene = new Scene({lpMod: setupMod()})
-    scene.start((scene) => {
-        let a = true
-        let b = false
-        let c = a && b && true && a && b
-        let d = a && true
+    const lpMod = setupMod()
+    let scene = new Scene({lpMod, modsDir: lpMod.modsDir, name: 'boolean_and'}, (scene) => {
+        scene.start(() => {
+            let a = true
+            let b = false
+            let c = a && b && true && a && b
+            let d = a && true
+        })
     })
-    await scene.writeFiles({buildPath: 'build', filePath: 'test/core/objects/LPBoolean', fileName: 'boolean_and_with_lhs_rhs_chaining', type: 'scene', debug: true})
-    expect(scene._code.trim()).toBe([
+    expect((await scene.toString()).trim()).toBe([
         'sceneStart()',
         '  a = true',
         '  b = false',
         '  c = a && b && true && a && b',
         '  d = a && true',
-        'sceneEnd()'
+        'sceneEnd()',
     ].join(LB))
 })
 
-test('boolean.or with lhs/rhs chaining', () => {
-    let scene = new Scene({lpMod: setupMod()})
-    scene.start((scene) => {
-        let a = true
-        let b = false
-        let c = a || b || true || (a && b)
+test('boolean.or with lhs/rhs chaining', async () => {
+    const lpMod = setupMod()
+    let scene = new Scene({lpMod, modsDir: lpMod.modsDir, name: 'boolean_or'}, (scene) => {
+        scene.start(() => {
+            let a = true
+            let b = false
+            let c = a || b || true || (a && b)
+        })
     })
-    expect(scene._code.trim()).toBe([
+    expect((await scene.toString()).trim()).toBe([
         'sceneStart()',
         '  a = true',
         '  b = false',
         '  c = a || b || true || a && b',
-        'sceneEnd()'
+        'sceneEnd()',
     ].join(LB))
 })
 
 test('boolean.not with chaining', async () => {
-    let scene = new Scene({lpMod: setupMod()})
-    scene.start((scene) => {
-        let a = true
-        let b = false
-        let c = !(a || b || true || a && !b)
+    const lpMod = setupMod()
+    let scene = new Scene({lpMod, modsDir: lpMod.modsDir, name: 'boolean_not'}, (scene) => {
+        scene.start(() => {
+            let a = true
+            let b = false
+            let c = !(a || b || true || a && !b)
+        })
     })
-    await scene.writeFiles({buildPath: 'build', filePath: 'test/core/objects/LPBoolean', fileName: 'boolean_not_with_chaining', type: 'scene', debug: true})
-    expect(scene._code.trim()).toBe([
+    expect((await scene.toString()).trim()).toBe([
         'sceneStart()',
         '  a = true',
         '  b = false',
         '  c = !(a || b || true || a && !(b))',
-        'sceneEnd()'
+        'sceneEnd()',
     ].join(LB))
+
 })
 
-test('boolean.paren/squareBrackets with lhs/rhs chaining', () => {
-    let scene = new Scene({lpMod: setupMod()})
-    scene.start((scene) => {
-        const {paren, squareBrackets} = scene;
-        let a = true
-        let b = false
-        let c = !paren(a || b && true) || squareBrackets(a && !b)
+test('boolean.paren/squareBrackets with lhs/rhs chaining', async () => {
+    const lpMod = setupMod()
+    let scene = new Scene({lpMod, modsDir: lpMod.modsDir, name: 'boolean_paren_squareBrackets'}, (scene) => {
+        scene.start(() => {
+            const {paren, squareBrackets} = scene;
+            let a = true
+            let b = false
+            let c = !paren(a || b && true) || squareBrackets(a && !b)
+        })
     })
-    expect(scene._code.trim()).toBe([
+    expect((await scene.toString()).trim()).toBe([
         'sceneStart()',
         '  a = true',
         '  b = false',
         '  c = !((a || b && true)) || [a && !(b)]',
-        'sceneEnd()'
+        'sceneEnd()',
     ].join(LB))
 })
