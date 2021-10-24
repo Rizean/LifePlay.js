8f8de0d441778632ee197efc5012b43a79e070fa
rizean <rizean@gmail.com>
2021-10-24 18:53:46 -0400

    added project level index for exports

---
+++ index.js
@@ -0,0 +1,17 @@
+// Exports
+
+const LPAction = require('./src/core/LPAction')
+const LPDescription = require('./src/core/LPDescription')
+const LPMod = require('./src/core/LPMod')
+const LPScene = require('./src/core/LPScene')
+const LPStat = require('./src/core/LPStat')
+const LPTalk = require('./src/core/LPTalk')
+
+module.exports = {
+    LPAction,
+    LPDescription,
+    LPMod,
+    LPScene,
+    LPStat,
+    LPTalk,
+}
